1d24e11389b089b47fda4a938ac8133ea6154d1d
Kirill_Gotwald <s.kirill10@gmail.com>
2024-07-03 21:42:52 +1000

    Черновое решение. 1.0 без цикла новой игры

--- main.py
+++ main.py
@@ -1,3 +1,87 @@
-print('123')
+from random import choice
+
+words_list = ['Автомобиль', 'Кот', 'Работа', 'Линукс', 'Завод', 'Здоровье']     # Банк слов
+cyrillic_lower_letters = list('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')      # Кирилический алфавит
+strike_letters = cyrillic_lower_letters[:]
+
+# # while True:
+#     print('_________________________________________________________________________________')
+#     print('Главное меню:')
+    
+#     main_menu = {
+#         1 : 'Начать игру',
+#         2 : '',
+#         3 : 'Выйти из игры',
+#     }
+    
+#     for k,v in main_menu.items():
+#         print(k,v)
+#     user_action = input()
+
+    
+#     if Counter_errors == 5:
+#         print('Вы ошиблись много раз. Хотите начать заного? Yes / No ')
+#         if input() in ['N', 'n', 'No', 'NO'] :
+#             print('Конец игры!')
+#             print('_________________________________________________________________________________')
+#             break
 
 
+# print('_________________________________________________________________________________')
+# print('Главное меню:')
+
+main_menu = {
+    1 : 'Начать игру',
+    2 : '',
+    3 : 'Выйти из игры',
+}
+
+for k,v in main_menu.items():
+    print(k,v)
+    
+#user_action = input()      # Выбор пользователя в меню
+user_action = '1'
+
+if user_action == '1':  # Если играем/новая игра
+    Word_in_game = choice(words_list)       # Рандомное слово из банка слов words_list, которое сейчас в игре
+    Secret_word = ['*'] * len(Word_in_game)     # Закрытое слово по длине слова в игре
+    Counter_errors = 0      # Счетчик ошибок
+    
+    print('____________________________________________________________')
+    print('Загаданное слово: ', *Secret_word)
+    
+    while True:
+        print('____________________________________________________________')
+        user_letter = input("Попробуйте угадать букву: ").lower()
+        print()
+        Check_Secret = Secret_word[:]       # Копирка спрятанного слова, для сверки изменений
+
+        for ind, letter in enumerate(Word_in_game):
+            if letter.lower() == user_letter:       # Если угадал в цикле
+                Secret_word[ind] = letter
+                if letter.lower() in strike_letters:
+                    strike_letters[strike_letters.index(letter.lower())] = '*'
+                    
+        if Check_Secret == Secret_word and user_letter not in ''.join(Secret_word).lower():     # Если не угадал
+            Counter_errors += 1
+            print(f'Вы ошиблись. Ошибки {Counter_errors}/7')
+        else:
+            print('ВЫ УГАДАЛИ БУКВУ')
+            print()
+        Check_Secret.clear()
+                            
+        print('Оставшиеся буквы алфавита: ')
+        print(*strike_letters)
+        print('____________________________________________________________') 
+        print(*Secret_word) 
+
+        if Counter_errors == 7:
+            break
+        elif '*' not in Secret_word:
+            print('____________________________________________________________')
+            print("Вы отгадали слово! Поздравляем!")
+            print('____________________________________________________________')
+            break
+
+    print(f'Игра завершена. Было загадано слово {Word_in_game}. Вы ошиблись {Counter_errors} раз.')
+    print('____________________________________________________________')
